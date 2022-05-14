b91be1772693ff8489a81d50cfe22cf63e8aeefe
Viki <vikiboss@qq.com>
2022-05-14 15:10:31 +0800

    🐛 fix gacha data fetching bug

--- src/render/pages/Gacha/index.tsx
+++ src/render/pages/Gacha/index.tsx
@@ -11,15 +11,22 @@ import styles from "./index.less";
 
 const Gocha: React.FC = () => {
   const notice = useNotice();
+  const [loading, setLoading] = useState<boolean>(false);
   const navigate = useNavigate();
   const [gacha, setGacha] = useState<Partial<GachaData>>({});
 
   const getUrl = async () => {
+    if (loading) return;
+    setLoading(true);
     const url = await nativeApi.getGachaUrl();
     if (url) {
       notice.info({ message: "正在拼命获取最新数据，请等待操作完成...", autoHide: false });
       const data = await nativeApi.getGachaListByUrl(url);
-      if (data?.list?.length) notice.success({ message: "祈愿数据获取完毕" });
+      if (data?.list?.length) {
+        notice.success({ message: "祈愿数据获取完毕" });
+      } else {
+        notice.faild({ message: "数据异常" });
+      }
       setGacha(data);
     } else {
       notice.faild({
@@ -27,6 +34,7 @@ const Gocha: React.FC = () => {
         duration: 3000
       });
     }
+    setLoading(false);
   };
   return (
     <>
