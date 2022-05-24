b3f7d35a1e5d4d9b5f2bda5e7e7e5166757ba00e
Viki <vikiboss@qq.com>
2022-05-24 11:52:23 +0800

    💄 modify home page UI

--- src/render/pages/Home/index.tsx
+++ src/render/pages/Home/index.tsx
@@ -43,16 +43,17 @@ import type { SignInfo } from "../../../services/getBBSSignInfo";
 import styles from "./index.less";
 
 const formatTime = (seconds: number) => {
-  if (seconds <= 60) return `${seconds}秒`;
-  if (seconds <= 3600) return `${Math.ceil(seconds / 60)}分钟`;
+  if (seconds <= 60) return `${seconds} 秒 `;
+  if (seconds <= 3600) return `${Math.ceil(seconds / 60)} 分钟 `;
   if (seconds <= 86400) {
-    const hour = `${Math.floor(seconds / 3600)}小时`;
+    const hour = `${Math.floor(seconds / 3600)} 小时 `;
     const minute = Math.ceil((seconds % 3600) / 60);
-    return hour + (minute ? minute + "分钟" : "");
+    return hour + (minute ? minute + " 分钟 " : "");
   }
-  const day = `${Math.floor(seconds / 86400)}天`;
+  const day = `${Math.floor(seconds / 86400)} 天 `;
   const hour = Math.ceil((seconds % 86400) / 3600);
-  return day + (hour ? hour + "小时" : "");
+  const timeStr = day + (hour ? hour + " 小时 " : "");
+  return timeStr.trim();
 };
 
 const Home: React.FC = () => {
@@ -140,39 +141,40 @@ const Home: React.FC = () => {
     }
   ];
 
+  // 处理原粹数值数据
   const isResinOk = note?.current_resin === note?.max_resin;
   const resinStatus = `${note?.current_resin}/${note?.max_resin}`;
-  const resinTitle = isResinOk
-    ? "树脂已全部恢复完毕"
-    : `树脂全部恢复预计需要${formatTime(Number(note?.resin_recovery_time))}`;
+  const resinTime = Number(note?.resin_recovery_time) || 0;
+  const resinTitle = isResinOk ? "树脂恢复完毕" : `树脂全部恢复预计需要 ${formatTime(resinTime)}`;
 
+  // 处理洞天宝钱数据
   const isHomeOk = note?.max_home_coin !== 0;
+  const isFull = note?.current_home_coin === note?.max_home_coin;
   const homeStatus = isHomeOk ? `${note?.current_home_coin}/${note?.max_home_coin}` : "暂未开启";
+  const homeTime = Number(note?.home_coin_recovery_time) || 0;
   const homeTitle = isHomeOk
-    ? `达储存上限预计需要${formatTime(Number(note?.home_coin_recovery_time))}`
-    : "尘歌壶相关功能暂未解锁或开启";
+    ? isFull
+      ? "洞天宝钱已存满"
+      : `存满预计需要 ${formatTime(homeTime)}`
+    : "尘歌壶功能未开启";
 
+  // 处理每日委托数据
   const hasReceivedTask = note?.total_task_num !== 0;
-  const taskStatus = hasReceivedTask
-    ? `${note?.finished_task_num}/${note?.total_task_num}`
-    : "尚未接取";
+  const taskRate = `${note?.finished_task_num}/${note?.total_task_num}`;
+  const taskStatus = hasReceivedTask ? taskRate : "尚未接取";
   const isTaskDone = note?.finished_task_num === note?.total_task_num;
+  const doneText = isTaskDone ? "已完成" : "未完成";
   const hasReceivedReward = note?.is_extra_task_reward_received;
-  const taskTitle =
-    "每日委托任务" +
-    (hasReceivedTask
-      ? isTaskDone
-        ? hasReceivedReward
-          ? "已完成，奖励已领取"
-          : "已完成，奖励未领取"
-        : "待完成"
-      : "尚未接取");
+  const extraText = hasReceivedReward ? "已领取" : "未领取";
+  const taskTitle = `每日委托任务${doneText}，额外奖励${extraText}`;
+
+  // 处理周本数据
   const discountStatus = `${note?.remain_resin_discount_num}/${note?.resin_discount_num_limit}`;
   const isDiscountDone = note?.remain_resin_discount_num === 0;
-  const discountTitle =
-    "本周树脂减半次数" +
-    (isDiscountDone ? " 已达上限" : `还剩${note?.remain_resin_discount_num}次`);
+  const discountText = isDiscountDone ? "已达上限" : `还剩 ${note?.remain_resin_discount_num} 次`;
+  const discountTitle = "本周树脂减半次数" + discountText;
 
+  // 处理参量质变仪数据
   const hasTransformer = note?.transformer?.obtained;
   const _ = note?.transformer?.recovery_time;
   const transformerTime = _.Day * 86400 + _.Hour * 3600 + _.Minute * 60 + _.Second;
@@ -180,17 +182,12 @@ const Home: React.FC = () => {
   const transformerStatus = hasTransformer
     ? isTransformerReady
       ? "已就绪"
-      : "冷却中"
+      : `冷却中`
     : "暂未获得";
-  const transformerTitle =
-    "参量质变仪" +
-    (hasTransformer
-      ? isTransformerReady
-        ? "已就绪"
-        : `冷却中，还剩 「${formatTime(transformerTime)}」`
-      : "暂未获得");
+  const transformerTitle = `距下次可用还剩 ${formatTime(transformerTime)}`;
 
-  const signStatus = `${sign.is_sign ? "已签" : "未签"}，本月累计${sign.total_sign_day}天`;
+  // 处理签到数据
+  const signStatus = `${sign.is_sign ? "已签" : "未签"}，累计${sign.total_sign_day}天`;
   const signTitle = `本月累计签到 ${sign.total_sign_day} 天，错过 ${sign.sign_cnt_missed} 天`;
 
   const notes = [
@@ -234,21 +231,17 @@ const Home: React.FC = () => {
 
   const dispatchs = (note?.expeditions || []).map((e) => {
     const done = e.status === "Finished";
-    return {
-      done,
-      avatar: e.avatar_side_icon,
-      title: done
-        ? "探索派遣任务已完成，等待领取"
-        : `探险中，距离探险结束还剩 「${formatTime(Number(e.remained_time))}」`
-    };
+    const doneText = "探索派遣任务已完成，等待领取";
+    const pendingText = `探险中，距离探险结束还剩 ${formatTime(Number(e.remained_time))}`;
+    const title = done ? doneText : pendingText;
+    const avatar = e.avatar_side_icon;
+    return { done, avatar, title };
   });
 
   const dispatchDetail = dispatchs.length
     ? `探索派遣 ${dispatchs.length}/${note?.max_expedition_num}`
     : "探索派遣 暂未派遣任何角色";
-  const dispatcTitle = dispatchs.length
-    ? `探索派遣 「${dispatchs.length}/${note?.max_expedition_num}」`
-    : "探索派遣 暂未派遣任何角色";
+  const dispatcTitle = dispatchDetail;
 
   const btns = [
     {
@@ -293,6 +286,11 @@ const Home: React.FC = () => {
     }
   ];
 
+  const handleUidClick = (uid: string) => {
+    nativeApi.writeClipboardText(uid);
+    notice.success({ message: "已将 UID 复制到剪切板" });
+  };
+
   return (
     <>
       <div className={styles.container}>
@@ -308,7 +306,12 @@ const Home: React.FC = () => {
                     info.map((e) => (
                       <div className={styles.infoItem} key={e.key}>
                         <span>{e.name}：</span>
-                        <div className={styles[e.key]}>{e.content}</div>
+                        <div
+                          className={styles[e.key]}
+                          onClick={e.key === "uid" ? handleUidClick.bind(null, e.content) : null}
+                        >
+                          {e.content}
+                        </div>
                       </div>
                     ))}
                 </div>
