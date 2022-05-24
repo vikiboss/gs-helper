b9086dff2f01e1b6349ec462a3bf269b29b308d3
Viki <vikiboss@qq.com>
2022-05-24 13:47:07 +0800

    💄 modify about page UI

--- src/render/pages/About/index.tsx
+++ src/render/pages/About/index.tsx
@@ -3,11 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { TiArrowBack } from "react-icons/ti";
 
 import CircleButton from "../../components/CircleButton";
-
-import styles from "./index.less";
-import rewardCode from "../../../assets/reward.jpg";
+import rewardWXCode from "../../../assets/reward.jpg";
+import groupQRCode from "../../../assets/group-qrcode.png";
 import { LINK_GITHUB_REPO } from "../../../constants";
 import nativeApi from "../../utils/nativeApi";
+
+import styles from "./index.less";
 
 const About: React.FC = () => {
   const navigate = useNavigate();
@@ -30,13 +31,13 @@ const About: React.FC = () => {
           <a href={licenceLink} target='_blank'>
             {" MIT "}
           </a>
-          协议开源，仅供个人学习交流使用，
+          协议开放源代码，仅供个人学习交流使用，
           <b>请勿用于任何商业或违法违规用途</b>
           ，二次修改请自觉遵守该协议的相关约定。
         </p>
 
         <p>
-          软件开发的初衷是将原神玩家所需要的绝大多数功能整合到一起，方便玩家更好的进行游戏，提升玩家的游戏效率和体验。数据可能存在延迟，请以游戏内数据为准。
+          开发初衷是将原神玩家所需要的多数功能整合到一起，提升玩家的游戏效率和体验。数据可能存在延迟，请以游戏内的实时数据为准。
         </p>
 
         <p>
@@ -44,23 +45,23 @@ const About: React.FC = () => {
           <a href='https://www.mihoyo.com/' target='_blank'>
             {" 「米哈游」 "}
           </a>
-          或 「原作者」 所有。如有发现任何侵权行为，并持有相关证据，请联系开发者删除。
+          或 「原作者」 所有。如有发现任何侵权行为，并持有有效证明，请联系开发者对相关内容进行删除。
         </p>
       </div>
-      <div className={styles.thank}>{"= 开发不易 感谢支持 ="}</div>
+
       <div className={styles.bottom}>
         <div className={styles.items}>
+          <div className={styles.item}>※ 当前版本：{vsersion}</div>
           <div className={styles.item}>
             <div className={styles.field}>
-            ※ 开发者：
+              ※ 开发者：
               <a href={githubLink} target='_blank'>
                 Viki
               </a>
             </div>
-            <div className={styles.field}>※ 当前版本：{vsersion}</div>
           </div>
           <div className={styles.item}>
-          ※ 源码：
+            ※ 源码：
             <a href={LINK_GITHUB_REPO} target='_blank'>
               {" GitHub "}
             </a>
@@ -68,18 +69,23 @@ const About: React.FC = () => {
           </div>
           <div className={styles.item}>
             <div className={styles.field}>
-            ※ 交流群：
+              ※ 交流群：
               <a href={groupLink} target='_blank'>
                 176593098
               </a>
             </div>
           </div>
         </div>
-        <div className={styles.reward}>
-          <img className={styles.rewardCode} src={rewardCode} alt='微信赞赏码' />
+        <div className={styles.codeZone}>
+          <img className={styles.code} src={groupQRCode} alt='交流群' />
+          <div>「原神助手」 交流群</div>
+        </div>
+        <div className={styles.codeZone}>
+          <img className={styles.code} src={rewardWXCode} alt='微信赞赏码' />
           <div>Viki の 微信赞赏码</div>
         </div>
       </div>
+      <div className={styles.thank}>{"※ 开发不易 ❤ 感谢支持 ※"}</div>
       <CircleButton
         Icon={TiArrowBack}
         size='middle'
