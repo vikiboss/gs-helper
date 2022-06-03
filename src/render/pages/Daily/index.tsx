3741088fedcd8eed273131f060104c6371557768
Viki <vikiboss@qq.com>
2022-06-03 21:27:51 +0800

    💄 modify daily page UI

--- src/render/pages/Daily/index.tsx
+++ src/render/pages/Daily/index.tsx
@@ -4,18 +4,33 @@ import cn from "classnames";
 import React, { useEffect, useState } from "react";
 
 import CircleButton from "../../components/CircleButton";
+import Loading from "../../components/Loading";
 import nativeApi from "../../utils/nativeApi";
+import useNotice from "../../hooks/useNotice";
 import withAuth from "../../auth/withAuth";
 
 import type { CalenderEvent } from "../../../services/getCalenderList";
 
 import styles from "./index.less";
 
+type Type = "roles" | "weapons" | "materials";
+
 const WeekMap = ["日", "一", "二", "三", "四", "五", "六"];
+
 const Types = [
-  { title: "可培养角色", name: "roles" },
-  { title: "可突破武器", name: "weapons" },
-  { title: "可获取素材", name: "materials" }
+  { title: "天赋培养", name: "roles" },
+  { title: "武器突破", name: "weapons" },
+  { title: "突破素材", name: "materials" }
+];
+
+const Tips = [
+  "「周日」 所有的材料均开放获取",
+  "「周一」 与 「周四」 可获取的材料相同",
+  "「周二」 与 「周五」 可获取的材料相同",
+  "「周三」 与 「周六」 可获取的材料相同",
+  "「周一」 与 「周四」 可获取的材料相同",
+  "「周二」 与 「周五」 可获取的材料相同",
+  "「周三」 与 「周六」 可获取的材料相同"
 ];
 
 const getUniqueArray = (arr: any[], key: string) => {
@@ -27,11 +42,7 @@ const getUniqueArray = (arr: any[], key: string) => {
       res.push(e);
     }
   }
-  return res.sort((p, n) => {
-    const pn = Number(p.title.includes("「"));
-    const nn = Number(n.title.includes("「"));
-    return pn - nn;
-  });
+  return res;
 };
 
 const getMaterialList = (calenderList: CalenderEvent[]) => {
@@ -57,10 +68,11 @@ const getMaterialList = (calenderList: CalenderEvent[]) => {
 const Daily: React.FC = () => {
   const todayWeek = new Date().getDay();
   const navigate = useNavigate();
-  const [changing, setChanging] = useState<boolean>(false);
-  const [type, setType] = useState<string>("roles");
-  const [week, setWeek] = useState<number>(todayWeek);
+  const notice = useNotice();
   const [calenderList, setCalenderList] = useState<CalenderEvent[]>([]);
+  const [changing, setChanging] = useState<boolean>(false);
+  const [type, setType] = useState<Type>("roles");
+  const [week, setWeek] = useState<number>(todayWeek);
 
   useEffect(() => {
     (async () => {
@@ -70,11 +82,13 @@ const Daily: React.FC = () => {
   }, []);
 
   const roles = calenderList.filter((e) => e.break_type === "2");
+  roles.sort((p, n) => JSON.parse(p.sort)[0] - JSON.parse(n.sort)[0]);
   const weapons = calenderList.filter((e) => e.break_type === "1");
+  weapons.sort((p, n) => JSON.parse(p.sort)[0] - JSON.parse(n.sort)[0]);
   const materials = getMaterialList(calenderList);
+  materials.sort((p, n) => JSON.parse(p.sort)[0] - JSON.parse(n.sort)[0]);
   const map: Record<string, CalenderEvent[]> = { roles, weapons, materials };
-  const list = map[type];
-  const today = list.filter((e) => e.drop_day.includes(String(((week + 6) % 7) + 1)));
+  const list = map[type].filter((e) => e.drop_day.includes(String(((week + 6) % 7) + 1)));
   const todayClass = cn(styles.btn, todayWeek === week ? styles.active : "");
 
   const handleChange = async (fn: Function) => {
@@ -83,57 +97,77 @@ const Daily: React.FC = () => {
     setTimeout(() => setChanging(false), 20);
   };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.title}>今日素材一览</div>
-      <div className={styles.content}>
-        <div className={styles.contentTop}>
-          <div className={styles.weeks}>
-            <div className={todayClass} onClick={() => handleChange(setWeek.bind(null, todayWeek))}>
-              今日
-            </div>
-            <span>|</span>
-            {WeekMap.map((e, i) => (
-              <div
-                key={e}
-                className={cn(styles.btn, i === week ? styles.active : "")}
-                onClick={() => handleChange(setWeek.bind(null, i))}
-              >
-                周{e}
-              </div>
-            ))}
-          </div>
-          <div className={styles.types}>
-            {Types.map((e) => (
-              <div
-                key={e.name}
-                className={cn(styles.btn, e.name === type ? styles.active : "")}
-                onClick={() => handleChange(setType.bind(null, e.name))}
-              >
-                {e.title}
-              </div>
-            ))}
-          </div>
-        </div>
-        {!changing && (
-          <div className={styles.main}>
-            {today.map((e) => (
-              <div key={e.title}>
-                <img src={e.img_url} alt={e.title} />
-                <span>{e.title}</span>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+  const handleItemClick = (e: CalenderEvent) => {
+    let message = type === "roles" ? `「${e.title}」 天赋培养需要：` : "";
+    message += type !== "materials" ? `${e.contentInfos.map((e) => e.title).join("、")}` : e.title;
+    message += `，可在 「${e.contentSource[0]?.title || "忘却之峡"}」 获取`;
+    notice.info({ message });
+  };
 
-      <CircleButton
-        Icon={TiArrowBack}
-        size='middle'
-        className={styles.backBtn}
-        onClick={() => navigate("/")}
-      />
-    </div>
+  return (
+    <>
+      <div className={styles.container}>
+        {calenderList.length > 0 ? (
+          <>
+            <div className={styles.title}>日历</div>
+            <div className={styles.content}>
+              <div className={styles.contentTop}>
+                <div className={styles.weeks}>
+                  <div
+                    className={todayClass}
+                    onClick={() => handleChange(setWeek.bind(null, todayWeek))}
+                  >
+                    今日
+                  </div>
+                  <span>|</span>
+                  {WeekMap.map((e, i) => (
+                    <div
+                      key={e}
+                      className={cn(styles.btn, i === week ? styles.active : "")}
+                      onClick={() => handleChange(setWeek.bind(null, i))}
+                    >
+                      周{e}
+                    </div>
+                  ))}
+                </div>
+                <span>{Tips[week]}</span>
+                <div className={styles.types}>
+                  {Types.map((e) => (
+                    <div
+                      key={e.name}
+                      className={cn(styles.btn, e.name === type ? styles.active : "")}
+                      onClick={() => handleChange(setType.bind(null, e.name))}
+                    >
+                      {e.title}
+                    </div>
+                  ))}
+                </div>
+              </div>
+              {!changing && (
+                <div className={styles.main}>
+                  {list.map((e) => (
+                    <div key={e.title} onClick={() => handleItemClick(e)}>
+                      <img src={e.img_url} alt={e.title} />
+                      <span>{e.title}</span>
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <Loading />
+        )}
+
+        <CircleButton
+          Icon={TiArrowBack}
+          size='middle'
+          className={styles.backBtn}
+          onClick={() => navigate("/")}
+        />
+      </div>
+      {notice.holder}
+    </>
   );
 };
 
