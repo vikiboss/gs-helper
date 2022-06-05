637fe27aa73887ed0854e74597ffcd2a4820b2fd
Viki <vikiboss@qq.com>
2022-06-05 13:44:30 +0800

    💄 complete role detail page UI

--- src/render/pages/Role/index.tsx
+++ src/render/pages/Role/index.tsx
@@ -3,7 +3,7 @@ import { TiArrowBack } from "react-icons/ti";
 import { useNavigate } from "react-router-dom";
 import cn from "classnames";
 
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 import { ElementTypes } from "../../../constants";
 import Button from "../../components/Button";
@@ -12,6 +12,8 @@ import Loading from "../../components/Loading";
 import nativeApi from "../../utils/nativeApi";
 import useNotice from "../../hooks/useNotice";
 import withAuth from "../../auth/withAuth";
+
+import lock from "../../../assets/lock.png";
 
 import star1 from "../../../assets/star1.png";
 import star2 from "../../../assets/star2.png";
@@ -28,12 +30,13 @@ import Cryo from "../../../assets/cryo.png";
 import Dendro from "../../../assets/dendro.png";
 
 import type { PublicRole } from "../../../services/getPublicRoleList";
-import type { Role as RoleInfo } from "../../../services/getOwnedRoleList";
+import type { Reliquarie, Role as RoleInfo } from "../../../services/getOwnedRoleList";
 
 import styles from "./index.less";
 
 type RenderRoleInfo = RoleInfo & PublicRole;
 type TabType = "weapon" | "reliquary" | "constellation" | "profile";
+type ReliquaryEffect = { name: string; effects: { num: number; effect: string }[] };
 
 const StarImgs: string[] = [star1, star2, star3, star4, star5];
 const ElementImgs: Record<string, string> = { Pyro, Hydro, Anemo, Electro, Geo, Cryo, Dendro };
@@ -48,6 +51,7 @@ const TabMap: Record<TabType, string> = {
 const getStarClass = (rarity: number) => styles[`star${rarity > 5 ? 6 : rarity}`];
 const getStarImage = (rarity: number) => StarImgs[(rarity > 5 ? 5 : rarity) - 1];
 
+// 将获取的个人角色信息和公开的角色的信息合并
 const getFullRoleInfo = (roles: RoleInfo[], publickRoles: PublicRole[]): RenderRoleInfo[] => {
   const res = [];
   for (const role of roles) {
@@ -82,13 +86,45 @@ const getFullRoleInfo = (roles: RoleInfo[], publickRoles: PublicRole[]): RenderR
   return res;
 };
 
+// 圣遗物套装效果转换算法
+const getReliquaryEffects = (reliquaries: Reliquarie[]): ReliquaryEffect[] => {
+  const effects: ReliquaryEffect[] = [];
+  // 对每一个圣遗物进行遍历
+  for (const e of reliquaries) {
+    const isExist = effects.map((e) => e.name).includes(e.set.name);
+    // 如果该系列已经处理过，则跳过
+    if (isExist) continue;
+    // 没处理则往下处理，先获取圣遗物套装名称
+    const effectName = e.set.name;
+    // 获取需要触发套装效果的数目列表，一般是 2、4
+    const effectNums = e.set.affixes.map((e) => e.activation_number);
+    // 获取已装配的该系列套装的数目
+    const setNum = reliquaries.filter((e) => e.set.name === effectName).length;
+    // 声明一个套装系列效果的列表 { num: number, effect: string }
+    const _effects = [];
+    // 遍历触发套装效果的数目列表，依次判断是否达到数目要求
+    for (const num of effectNums) {
+      // 达到数目要求则视为有效效果，加入到套装系列效果的列表
+      if (setNum >= num) {
+        const affiex = e.set.affixes.filter((e) => e.activation_number === num)[0];
+        _effects.push({ num: affiex.activation_number, effect: affiex.effect });
+      }
+    }
+    // 如果该系列存在有效的效果，则将该系列的圣遗物效果加入到总效果列表中
+    if (_effects.length > 0) effects.push({ name: e.set.name, effects: _effects });
+  }
+  // 返回套装总效果
+  return effects;
+};
+
 const Role: React.FC = () => {
   const navigate = useNavigate();
   const notice = useNotice();
   const [index, setIndex] = useState<number>(0);
-  const [infoTab, setInfoTab] = useState<TabType>("reliquary");
+  const [constellIndex, setConstellIndex] = useState<number>(0);
+  const [infoTab, setInfoTab] = useState<TabType>("weapon");
   const [isRoleChanging, setIsRoleChanging] = useState<boolean>(true);
-  const [mode, setMode] = useState<"detail" | "list">("detail");
+  const [mode, setMode] = useState<"detail" | "list">("list");
   const [publicRoles, setPublicRolos] = useState<PublicRole[]>([]);
   const [roles, setRoles] = useState<RoleInfo[]>([]);
 
@@ -125,6 +161,7 @@ const Role: React.FC = () => {
   const _roles = getFullRoleInfo(roles, publicRoles);
   const isDetail = mode === "detail" && _roles.length > 0;
   const currentRole = _roles[index];
+  const reliquaryEffects = isDetail ? getReliquaryEffects(_roles[index].reliquaries) : [];
 
   const toggleMode = () => setMode(isDetail ? "list" : "detail");
 
@@ -195,6 +232,9 @@ const Role: React.FC = () => {
                           <span>{currentRole.fetter}</span>
                         </>
                       )}
+                      {currentRole.actived_constellation_num > 0 && (
+                        <span>{currentRole.actived_constellation_num}命</span>
+                      )}
                     </div>
                     <div className={styles.tabContainer}>
                       <div className={styles.tab}>
@@ -241,28 +281,81 @@ const Role: React.FC = () => {
                         {/* 渲染圣遗物 Tab */}
                         {infoTab === "reliquary" && (
                           <div className={styles.reliquary}>
-                            {currentRole.reliquaries.length ? (
-                              <>
-                                {currentRole.reliquaries.map((e) => (
-                                  <div
-                                    className={cn(styles.reliquaryItem, styles[`star-${e.rarity}`])}
-                                    key={e.pos}
-                                  >
-                                    <img src={e.icon} alt='icon' />
-                                    <img src={getStarImage(e.rarity)} alt='star' />
-                                    <span>{e.level}</span>
-                                    <span>{e.name}</span>
-                                  </div>
-                                ))}
-                              </>
-                            ) : (
-                              <div>未装配任何圣遗物</div>
-                            )}
+                            <div>
+                              {currentRole.reliquaries.length ? (
+                                <>
+                                  {currentRole.reliquaries.map((e) => (
+                                    <div
+                                      className={cn(
+                                        styles.reliquaryItem,
+                                        styles[`star-${e.rarity}`]
+                                      )}
+                                      key={e.pos}
+                                    >
+                                      <img src={e.icon} alt='icon' />
+                                      <img src={getStarImage(e.rarity)} alt='star' />
+                                      {e.level > 0 && <span>{e.level}</span>}
+                                      <span>{e.name}</span>
+                                    </div>
+                                  ))}
+                                </>
+                              ) : (
+                                <div>未装配任何圣遗物</div>
+                              )}
+                            </div>
+                            <div>
+                              {reliquaryEffects.length
+                                ? reliquaryEffects.map((e) => (
+                                    <Fragment key={e.name}>
+                                      <span>{e.name}</span>
+                                      <div>
+                                        {e.effects.map((e) => (
+                                          <span key={e.num}>
+                                            ·{e.num}件套：{e.effect}
+                                          </span>
+                                        ))}
+                                      </div>
+                                    </Fragment>
+                                  ))
+                                : currentRole.reliquaries.length > 0 && (
+                                    <div>当前圣遗物没有套装效果</div>
+                                  )}
+                            </div>
                           </div>
                         )}
                         {/* 渲染命座 Tab */}
                         {infoTab === "constellation" && (
-                          <div className={styles.constellation}>命座</div>
+                          <div className={styles.constellation}>
+                            <div>
+                              {currentRole.constellations.map((e, i) => {
+                                return (
+                                  <img
+                                    className={constellIndex === i ? styles.constelllActive : ""}
+                                    key={e.name + i}
+                                    src={e.is_actived ? e.icon : lock}
+                                    alt={"命座" + e.pos}
+                                    onClick={() => setConstellIndex(i)}
+                                  />
+                                );
+                              })}
+                            </div>
+                            <div>
+                              <span>{currentRole.constellations[constellIndex].name}</span>
+                              <span>命之座·第{constellIndex + 1}层</span>
+                              <div>
+                                {currentRole.constellations[constellIndex].effect
+                                  .replace(/<(.*?)>/g, "")
+                                  .split("\\n")
+                                  .map((e) => (
+                                    <div key={e}>{e}</div>
+                                  ))}
+                              </div>
+                              {/* <span>{e.name}</span> */}
+                              {/* {.map((e) => (
+                                      <div key={e}>{e}</div>
+                                ))} */}
+                            </div>
+                          </div>
                         )}
                         {/* 渲染简介 Tab */}
                         {infoTab === "profile" && (
