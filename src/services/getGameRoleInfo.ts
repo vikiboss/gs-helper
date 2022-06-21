9587edb16d3ac2ea4e7e5b29d0356b61fb99de7d
Viki <vikiboss@qq.com>
2022-06-21 08:47:15 +0800

    🐛 fix bugs triggered by cookie expiration

--- src/services/getGameRoleInfo.ts
+++ src/services/getGameRoleInfo.ts
@@ -6,7 +6,7 @@ import type { GameRole } from "../typings";
 const getGameRoleInfo = async (): Promise<GameRole | null> => {
   const currentUser = getCurrentUser();
   const roles = await getUserRolesByCookie(currentUser.cookie);
-  return roles[0] || null;
+  return (roles && roles[0]) || null;
 };
 
 export default getGameRoleInfo;
