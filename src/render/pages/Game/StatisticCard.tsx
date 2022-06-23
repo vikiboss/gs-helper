62a0f7591534aef90d647e9a9a1db27a665ca3cb
Viki <vikiboss@qq.com>
2022-06-23 18:20:44 +0800

    🚧 modify game data page UI

--- src/render/pages/Game/StatisticCard.tsx
+++ src/render/pages/Game/StatisticCard.tsx
@@ -1,12 +1,12 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import { deepClone } from "../../../utils/utils";
+import BounceNumber from "../../components/BounceNumber";
+import getServerNameByServer from "../../../utils/getServerNameByServer";
 
 import type { GameRoleCardData } from "../../../services/getGameRoleCard";
 
 import styles from "./index.less";
-import getServerNameByServer from "../../../utils/getServerNameByServer";
-import BounceNumber from "../../components/BounceNumber";
 
 interface StatisticCardProp {
   data: GameRoleCardData & { uid: string };
@@ -14,11 +14,11 @@ interface StatisticCardProp {
 
 const StatisticCard: React.FC<StatisticCardProp> = (props) => {
   const { data } = props;
-  const we = deepClone(data.world_explorations);
+  const we = deepClone(data.world_explorations).reverse();
   const stats = data.stats;
   return (
     <div className={styles.statisticCard}>
-      <div className={styles.row}>
+      <div className={styles.column}>
         <div className={styles.stats}>
           <div>
             <div>{data.role.nickname}</div>
@@ -33,11 +33,11 @@ const StatisticCard: React.FC<StatisticCardProp> = (props) => {
             </div>
             <div>
               <BounceNumber size={24} number={stats.achievement_number} />
-              <span>成就数</span>
+              <span>成就</span>
             </div>
             <div>
               <BounceNumber size={24} number={stats.avatar_number} />
-              <span>角色数</span>
+              <span>角色</span>
             </div>
             <div>
               <BounceNumber size={24} number={stats.way_point_number} />
@@ -88,6 +88,56 @@ const StatisticCard: React.FC<StatisticCardProp> = (props) => {
             </div>
           </div>
         </div>
+        <div className={styles.homes}>
+          <div>
+            <span>尘歌壶</span>
+            {data.homes.length > 0 && (
+              <div>
+                <img
+                  src={data.homes[0].comfort_level_icon}
+                  alt={data.homes[0].comfort_level_name}
+                />
+                <span>{data.homes[0].comfort_level_name}</span>
+              </div>
+            )}
+          </div>
+          {data.homes.length > 0 ? (
+            <>
+              <div>
+                {data.homes.map((e) => (
+                  <div key={e.name}>
+                    <img src={e.icon} alt={e.name}></img>
+                    <span>{e.name}</span>
+                  </div>
+                ))}
+              </div>
+              <div>
+                <div>
+                  <div>
+                    <BounceNumber size={24} number={data.homes[0].level} />
+                    <span>信任等阶</span>
+                  </div>
+                  <div>
+                    <BounceNumber size={24} number={data.homes[0].comfort_num} />
+                    <span>最高洞天仙力</span>
+                  </div>
+                  <div>
+                    <BounceNumber size={24} number={data.homes[0].item_num} />
+                    <span>获得摆设</span>
+                  </div>
+                  <div>
+                    <BounceNumber size={24} number={data.homes[0].visit_num} />
+                    <span>历史访客</span>
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <div style={{ lineHeight: "120px" }}>尘歌壶未开启</div>
+          )}
+        </div>
+      </div>
+      <div className={styles.column}>
         <div className={styles.avatars}>
           <div>常用角色</div>
           <div>
@@ -95,49 +145,42 @@ const StatisticCard: React.FC<StatisticCardProp> = (props) => {
               <div key={e.id}>
                 <img src={e.card_image} key={e.name} alt={e.name} />
                 <div>
+                  <span>{e.name}</span>
                   <span>
-                    {e.name} Lv.{e.level} {e.actived_constellation_num}命
-                  </span>
-                  <span>
-                    {e.rarity}星 好感度：{e.fetter}
+                    {/* {e.actived_constellation_num}命 好感{e.fetter} */}
+                    Lv.{e.level}
                   </span>
                 </div>
               </div>
             ))}
           </div>
         </div>
-      </div>
-      <div className={styles.row}>
-        <div className={styles.homes}>
-          <div>尘歌壶</div>
-          <div>
-            {data.homes.map((e) => (
-              <div key={e.name}>
-                <img src={e.icon} alt={e.name}></img>
-                <span>{e.name}</span>
-              </div>
-            ))}
-          </div>
-          <div>
-            <div>
-
-            </div>
-          </div>
-        </div>
         <div className={styles.explorations}>
-          {we.reverse().map((e) => (
-            <div key={e.id}>
-              {/* <img src={e.icon} /> */}
-              {e.name}|{e.level}|{(e.exploration_percentage / 10).toFixed(1)}%
-            </div>
-          ))}
+          <div>世界探索</div>
+          <div>
+            {we.map((e) => {
+              const isR = e.type === "Reputation";
+              const name = !isR && (e.offerings[0]?.name || "").replaceAll("等级", "");
+              const text = isR ? "声望" : name || "";
+              const level = text ? `${text} ${e.level} 级` : "";
+              return (
+                <div key={e.id}>
+                  <img src={e.background_image} alt={e.name} />
+                  <div>
+                    <img src={e.icon} />
+                    <div>
+                      <span>{e.name}</span>
+                      <span>
+                        {e.exploration_percentage / 10}% {level}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
-      {/* <div>
-        {data.city_explorations.map((e, i) => (
-          <div key={i}>{e?.toString()}</div>
-        ))}
-      </div> */}
     </div>
   );
 };
--- src/render/pages/Game/index.tsx
+++ src/render/pages/Game/index.tsx
@@ -37,11 +37,12 @@ const Game: React.FC = () => {
         nativeApi.getGameRoleCard(uid),
         nativeApi.getSpiralAbyss(uid)
       ]);
-      if (!card.role?.nickname || !abyss.schedule_id) return;
+      if (!card?.role?.nickname || !abyss?.schedule_id) return false;
       setLoading(false);
       setCardData({ ...card, uid });
       setSpiralAbyss({ ...abyss, uid });
       console.log(card, abyss);
+      return true;
     } catch (e) {
       const isOffline = e?.message?.includes("getaddrinfo");
       const msg = isOffline ? "网络状况不佳，请检查后重试 T_T" : "加载超时，请检查网络连接 T_T";
@@ -51,18 +52,30 @@ const Game: React.FC = () => {
 
   const handleQuery = async () => {
     if (!uid) {
-      const text = await nativeApi.readClipboardText();
+      const text = (await nativeApi.readClipboardText()).trim();
       if (text) {
-        setUid(text.trim());
+        if (text.match(/^[0-9]{2,12}$/)) {
+          setUid(text.trim());
+        } else {
+          notice.warning({ message: "剪切板中的内容不是有效的 UID" });
+        }
       } else {
         notice.warning({ message: "剪切板内容为空" });
       }
     } else {
-      setLoading(true);
-      notice.info({ message: "小派蒙努力查询中...", autoHide: false });
-      await updateInfo(uid);
-      setLoading(false);
-      notice.success({ message: "数据获取成功" });
+      if (uid.match(/^[1-9][0-9]{7,9}$/)) {
+        setLoading(true);
+        notice.info({ message: "小派蒙努力查询中...", autoHide: false });
+        const isOK = await updateInfo(uid);
+        if (!isOK) {
+          notice.faild({ message: "目标 UID 不存在或数据未公开" });
+        } else {
+          notice.success({ message: "数据获取成功", duration: 1000 });
+        }
+        setLoading(false);
+      } else {
+        notice.warning({ message: "输入的 UID 无效，请检查" });
+      }
     }
   };
 
@@ -76,11 +89,11 @@ const Game: React.FC = () => {
               <div className={styles.inputArea}>
                 <Input
                   value={uid}
-                  onChange={(e) => setUid(e.target.value)}
+                  onChange={(e) => setUid(e.target.value.slice(0, 11))}
                   autoFocus
                   type='number'
-                  min='0'
-                  max='999999999'
+                  min={0}
+                  max={9999999999}
                   placeholder='查询 UID '
                 />
                 <Button text={uid === "" ? "粘贴" : "查询"} onClick={handleQuery} />
