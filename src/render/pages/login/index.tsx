5526226faeefe8f6b7b5809d2d97861ed1dc4b34
Viki <hi@viki.moe>
2022-10-23 16:12:55 +0800

    chore: shorten login tip duration time

--- src/render/pages/login/index.tsx
+++ src/render/pages/login/index.tsx
@@ -53,7 +53,7 @@ const Login: React.FC<LoginProp> = (props) => {
   }, []);
 
   const handleLogin = () => {
-    notice.success({ message: "正在打开登录页面...", duration: 6000 });
+    notice.success({ message: "正在打开登录页面...", duration: 1200 });
     nativeApi.loginByBBS();
   };
 
