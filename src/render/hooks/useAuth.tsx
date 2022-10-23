12d8e573c5ee28abc12592260df647071b6b03b0
Viki <hi@viki.moe>
2022-10-23 11:19:32 +0800

    chore: delete files

--- src/render/hooks/useAuth.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import AuthContext from "../auth/AuthContext";
-
-function useAuth() {
-  return React.useContext(AuthContext);
-}
-
-export default useAuth;
--- src/render/utils/nativeApi.ts
+++
@@ -1,7 +0,0 @@
-import { EXPOSED_API_FROM_ELECTRON } from "../../constants";
-
-import type { ElectronWindow } from "../../typings";
-
-const nativeApi = (window as ElectronWindow)[EXPOSED_API_FROM_ELECTRON];
-
-export default nativeApi;
