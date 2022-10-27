a244365fe4316d13367b0f2d95639d1405af653a
Viki <hi@viki.moe>
2022-10-27 15:33:14 +0800

    feat: go back in new opend window by right click

--- src/main/IPC/openWindow.ts
+++ src/main/IPC/openWindow.ts
@@ -3,22 +3,15 @@ import { app, BrowserWindow, BrowserWindowConstructorOptions } from "electron";
 import { APP_USER_AGENT_DESKTOP } from "../../constants";
 import { isDev, store } from "..";
 
-const wins = new Map<string, BrowserWindow>();
-
 const openWindow = async (
   _: Electron.IpcMainEvent,
   url: string,
   options: BrowserWindowConstructorOptions = {},
   UA?: string
 ): Promise<void> => {
-  if (wins.has(url)) {
-    return wins.get(url).show();
-  }
-
   const win = new BrowserWindow({
     width: 1300,
     height: 803,
-    show: false,
     autoHideMenuBar: true,
     backgroundColor: "#F9F6F2",
     alwaysOnTop: store.get("settings").alwaysOnTop ?? false,
@@ -29,19 +22,26 @@ const openWindow = async (
     win.removeMenu();
   }
 
-  wins.set(url, win);
-  win.once("ready-to-show", () => win.show());
-  win.on("close", () => wins.delete(url));
-
   const dom = win.webContents;
 
+  // 在窗口内跳转
   dom.setWindowOpenHandler((details) => {
     dom.loadURL(details.url);
     return { action: "deny" };
   });
 
+  // 设置 UA
   dom.setUserAgent(UA || APP_USER_AGENT_DESKTOP + app.getVersion());
+
+  // 加载页面
   dom.loadURL(url);
+
+  // 鼠标右键返回上一页面
+  dom.addListener("context-menu", () => {
+    if (dom.canGoBack()) {
+      dom.goBack();
+    }
+  })
 };
 
 export default openWindow;
