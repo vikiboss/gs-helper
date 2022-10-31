3ceb2dd29e4429fc7cb3f71962d7c9c5cc72631b
Viki <hi@viki.moe>
2022-10-31 15:22:37 +0800

    fix: fix type ambiguity error of window variable

--- src/render/utils/nativeApi.ts
+++ src/render/utils/nativeApi.ts
@@ -2,6 +2,6 @@ import { EXPOSED_API_FROM_ELECTRON } from '../../constants';
 
 import type { ElectronWindow } from '../../typings';
 
-const nativeApi = window as ElectronWindow[EXPOSED_API_FROM_ELECTRON];
+const nativeApi = (window as ElectronWindow)[EXPOSED_API_FROM_ELECTRON];
 
 export default nativeApi;
