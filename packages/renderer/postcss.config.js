70263bdd28c13a36c552a64076c10d736d2fed42
Viki <hi@viki.moe>
2023-04-15 16:58:32 +0800

    feat: remove tailwindcss

--- packages/renderer/postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: {
-    tailwindcss: require('./tailwind.config'),
-    autoprefixer: {}
-  }
-}
--- packages/renderer/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
-}
